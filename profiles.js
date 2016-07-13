6dae7f128ac2199e65720db4ddc303de743195a5
Sridhar <sreedharriyer@gmail.com>
2016-07-12 22:07:48 -0700

    json file

---
+++ profiles.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sridhar: {
+    name: "Sridhar Iyer",
+    irc: "isridhar" ,
+    twitter: "isridhar",
+    github: "sridharriyer",
+    location: "California USA",
+    description: "day dreamer"
+  }
+};
