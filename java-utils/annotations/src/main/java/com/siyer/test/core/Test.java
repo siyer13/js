0545e2c4d78a725706aa28b3c857b49652a519f4
Sridhar <sreedharriyer@gmail.com>
2019-01-12 17:26:54 +0530

    Custom Annotation

---
+++ java-utils/annotations/src/main/java/com/siyer/test/core/Test.java
@@ -0,0 +1,14 @@
+package com.siyer.test.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Test {
+
+    // should I ignore this test ?
+    public boolean enabled() default true;
+}
